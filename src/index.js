74e2c9ce992cacf96f21c8e03452434f529cf45b
Sergei Dukmas <dukmas08@gmail.com>
2024-10-10 14:40:16 +0300

    init:almost final

--- src/index.js
+++ src/index.js
@@ -1,19 +1,8 @@
+
 import demoPage, { mountDemoPage } from './pages/demo-page';
 
-function toggleDropdown(id) {
-  const dropdown = document.getElementById(`dropdown-${id}`);
-  dropdown.classList.toggle('show');
-}
 
-// Закрытие выпадающего списка при клике вне его
-window.onclick = function(event) {
-  if (!event.target.matches('.dropdown-btn')) {
-    const dropdowns = document.querySelectorAll('.dropdown-content');
-    dropdowns.forEach(dropdown => {
-      dropdown.classList.remove('show');
-    });
-  }
-}
+
 
 mountDemoPage();
 
--- src/pages/demo-page/index.js
+++ src/pages/demo-page/index.js
@@ -1,3 +1,4 @@
+
 const demoPage = (element) => {
   if (!element) {
     return;
@@ -22,6 +23,89 @@ document.querySelectorAll('input[type="checkbox"]').forEach(function(checkbox) {
       selectedList.appendChild(listItem);
     });
   });
+});
+
+
+// Инициализация ползунка Price Range
+var priceSlider = document.getElementById('price-slider');
+var priceValue = document.getElementById('price-value');
+
+noUiSlider.create(priceSlider, {
+  start: [234], // Начальное значение
+  range: {
+    'min': 0,
+    'max': 1000
+  }
+});
+
+// Обновление поля ввода при изменении ползунка
+priceSlider.noUiSlider.on('update', function (values, handle) {
+  priceValue.value = Math.round(values[handle]);
+});
+
+// Изменение положения ползунка при изменении значения в поле ввода
+priceValue.addEventListener('change', function () {
+  priceSlider.noUiSlider.set(this.value);
+});
+
+// Инициализация ползунка THC %
+var priceSlider = document.getElementById('price-slider');
+var priceValue = document.getElementById('price-value');
+
+noUiSlider.create(priceSlider, {
+  start: [234],
+  range: {
+    'min': 0,
+    'max': 1000
+  }
+});
+
+priceSlider.noUiSlider.on('update', function (values, handle) {
+  priceValue.value = Math.round(values[handle]);
+});
+
+priceValue.addEventListener('change', function () {
+  priceSlider.noUiSlider.set(this.value);
+});
+
+// Инициализация ползунка THC %
+var thcSlider = document.getElementById('thc-slider');
+var thcValue = document.getElementById('thc-value');
+
+noUiSlider.create(thcSlider, {
+  start: [20],
+  range: {
+    'min': 0,
+    'max': 100
+  }
+});
+
+thcSlider.noUiSlider.on('update', function (values, handle) {
+  thcValue.value = Math.round(values[handle]);
+});
+
+thcValue.addEventListener('change', function () {
+  thcSlider.noUiSlider.set(this.value);
+});
+
+// Инициализация ползунка CBD %
+var cbdSlider = document.getElementById('cbd-slider');
+var cbdValue = document.getElementById('cbd-value');
+
+noUiSlider.create(cbdSlider, {
+  start: [15],
+  range: {
+    'min': 0,
+    'max': 100
+  }
+});
+
+cbdSlider.noUiSlider.on('update', function (values, handle) {
+  cbdValue.value = Math.round(values[handle]);
+});
+
+cbdValue.addEventListener('change', function () {
+  cbdSlider.noUiSlider.set(this.value);
 });
 
 
